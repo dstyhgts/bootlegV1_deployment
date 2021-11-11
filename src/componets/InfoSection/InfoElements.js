e59519f8d95a8b72db37b96f40b5005574b8574b
Duncan Bielman <duncanbielman@Duncans-iMac.attlocal.net>
2021-11-11 12:13:26 -0800

    lilfixfive

--- src/componets/InfoSection/InfoElements.js
+++ src/componets/InfoSection/InfoElements.js
@@ -35,7 +35,7 @@ export const InfoContainer = styled.div`
 export const InfoWrapper = styled.div`
     display: grid;
     z-index: 1;
-    height: 100vh;
+    height: 1000px;
     width: 100%;
     max-width: 1100px;
     margin-right: auto;
